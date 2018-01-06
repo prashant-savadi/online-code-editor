26aaffd4a2ef4a224f64a5fea62ccdf76df1a058
prashant-savadi <prash.ec023@gmail.com>
2018-01-07 00:20:39 +0530

    base commit #5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,23 @@ import { CodingFilesComponent } from './coding-files/coding-files.component';
 import {TreeModule} from 'ng2-tree';
 import {MockServerService} from './mock-server.service';
 import {TreeModelService} from './tree-model.service';
-import {PerfectScrollbarModule} from 'ngx-perfect-scrollbar';
+import {PERFECT_SCROLLBAR_CONFIG, PerfectScrollbarModule} from 'ngx-perfect-scrollbar';
 import {FlexLayoutModule} from '@angular/flex-layout';
+import {AceEditorModule} from 'ng2-ace-editor';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatButtonModule, MatCardModule, MatCheckboxModule, MatFormFieldModule, MatIconModule, MatMenuModule, MatTabsModule,
+  MatToolbarModule
+} from '@angular/material';
+import {RouterTestingModule} from '@angular/router/testing';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {AppService} from './app.service';
+import {PerfectScrollbarConfigInterface} from 'ngx-perfect-scrollbar';
 
+const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+  suppressScrollX: true
+};
 
 @NgModule({
   declarations: [
@@ -21,10 +35,26 @@ import {FlexLayoutModule} from '@angular/flex-layout';
   imports: [
     BrowserModule,
     TreeModule,
+    AceEditorModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    HttpModule,
+    RouterTestingModule,
+    FormsModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
     PerfectScrollbarModule,
     FlexLayoutModule
   ],
-  providers: [MockServerService, TreeModelService],
+  providers: [AppService, MockServerService, TreeModelService, {
+    provide: PERFECT_SCROLLBAR_CONFIG,
+    useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
